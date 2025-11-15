a0bd18599efc431a16010cf42716e7c2b56eaec2
tsukkuku <bebracs@bk.ru>
2025-11-16 01:05:29 +0300

    feat: add Loader for Suspense fallback

--- src/app/provider/route-provider/index.tsx
+++ src/app/provider/route-provider/index.tsx
@@ -1,10 +1,24 @@
 import { router } from "@/app/routers";
 import { Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
+import { PropagateLoader } from "react-spinners";
 
 export const RouteProvider = () => {
   return (
-    <Suspense fallback={<h1>Loading...</h1>}>
+    <Suspense
+      fallback={
+        <PropagateLoader
+          color="var(--text-color)"
+          size={20}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        />
+      }
+    >
       <RouterProvider router={router} />
     </Suspense>
   );
